79dd954bfd803b80afe2d61b01a7f6d70eeb1b58
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-05-04 07:44:29 +0700

    Adds basic math script similar to accounting calc

--- lemath.js
+++ lemath.js
@@ -1,26 +1,116 @@
-let screen = document.querySelector(".calc-screen");
+let screen = document.querySelector(".screen");
+
+let currNum = document.querySelector(".current-number");
+let placeHolder = document.createElement("h1");
+let toBeAdded = document.querySelector(".number-to-be-added");
+let history = document.createElement("p")
+
+toBeAdded.textContent = "";
 
 const numberButton = document.querySelector(".numbers-buttons");
-
-let varA = document.createElement("p");
-let numA = "";
-let operations = "";
-let numB = ""
-numberButton.addEventListener("click", (event) => {
-    if (event.target.className == "calc-button") {
-        numA += event.target.textContent;
-        varA.textContent += event.target.textContent
-        screen.appendChild(varA);
-        console.log(varA);
-    }
-})
-
 const operationsButton = document.querySelector(".operations-buttons");
 
-operationsButton.addEventListener("click", (event) => {
-    if (event.target.className == "calc-button" || event.target.className == "calc-button big") {
-        console.log(event.target.textContent);
+
+let result;
+let eventFire = false;
+
+numberButton.addEventListener("click", (event) => {
+    if (event.target.className !== "numbers-buttons") {
+        currNum.textContent += event.target.textContent;
+        screen.appendChild(currNum);
+        if (!eventFire) {           
+            varA = currNum.textContent;
+            if (placeHolder) {
+                placeHolder.textContent = "";
+            }
+        }
+        if (eventFire) {
+            varB = currNum.textContent;
+            placeHolder.textContent = "";
+        }
+    }
+})  
+
+/* add decimal feature */
+
+const operation = document.querySelector(".operations-buttons");
+
+let varA = "";
+let varB = "";
+let currOper = "";
+let prevOper = "";
+
+
+operation.addEventListener("click", function operation (event) {
+    if (event.target.className !== "operations-buttons" && event.target.id !== "evaluate") {
+            toBeAdded.textContent += currNum.textContent + " " + event.target.textContent + " ";
+            eventFire = true;
+            screen.appendChild(toBeAdded);
+            if (currOper) {
+                prevOper = currOper;
+            }
+            currOper = event.target.textContent;         
+            screen.replaceChild(placeHolder, currNum);
+            currNum.textContent = "";
+            if (varA && varB && prevOper) {
+                switch (prevOper) {
+                    case "+":
+                        result = parseFloat(varA) + parseFloat(varB);
+                        varA = result;
+                        break;
+                    case "-":
+                        result = parseFloat(varA) - parseFloat(varB);
+                        varA = result;
+                        break;
+                    case "x":
+                        result = parseFloat(varA) * parseFloat(varB);
+                        varA = result;
+                        break;
+                    case "/":
+                        result = parseFloat(varA) / parseFloat(varB);
+                        varA = result;
+                        break;                        
+
+                }
+            }
+            placeHolder.textContent = varA;
+    }
+    if (event.target.id == "evaluate") {
+        switch (currOper) {
+            case "+":
+                result = parseInt(varA) + parseInt(varB);
+                toBeAdded.textContent += currNum.textContent ;
+                currNum.textContent = result;
+                break;
+            case "-":
+                result = parseInt(varA) - parseInt(varB);
+                toBeAdded.textContent += currNum.textContent ;
+                currNum.textContent = result;
+                break;
+            case "*":
+                console.log("eval timee")
+                result = parseInt(varA) * parseInt(varB);
+                toBeAdded.textContent += currNum.textContent ;
+                currNum.textContent = result;
+                break;
+            case "/":
+                console.log("eval timee")
+                result = parseInt(varA) / parseInt(varB);
+                toBeAdded.textContent += currNum.textContent ;
+                currNum.textContent = result;
+                break;
+        }
     }
 })
 
+const deleteButton = document.querySelector(".erase-buttons");
+
+deleteButton.addEventListener("click", (event) => {
+    if (event.target.id == "clear") {
+        currNum.textContent = "";
+    }
+    if (event.target.id == "erase") {
+        console.log("Not yet implemented");
+    }
+})
 
