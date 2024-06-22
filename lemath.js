2d0b2c686bff4d2745f1207fafd47bfd859a603f
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-06-22 07:09:34 +0700

    All of the functions implemented

--- lemath.js
+++ lemath.js
@@ -19,10 +19,9 @@ placeHolderNumber.setAttribute("class", "current-number");
 let negative = document.querySelector(".negative"); // for minus thingy
 negative.textContent = "";
 
-let varACopy = "";
-let varAText = "";
+let varACopy = varAEval = varAText = varBText = "";
 
-let varBText = "";
+let hasTyped = 0;
 
 let isError = false; // for errors such as divide by 0
 
@@ -30,32 +29,44 @@ let isError = false; // for errors such as divide by 0
 function inputNumber (event) {
     const condition = event.target.id !== "";
     const isNotDot = event.target.id !== "dot";
-    const isNotOpButton = event.target.className !== "calc-button operation-button"; // optimize this later
-    const currId = event.target.id;
+    const isNotOpButton = event.target.className !== "calc-button operation-button"; // optimize this late;
 
     const numbers = [1,2,3,4,5,6,7,8,9,0];
 
     // negate function
     if (condition && isNotDot && isNotOpButton) { // prevents inputting other than the numbers
-        if (currId === "negate") { // selects only the negate button
+        // to do
+        if (event.target.id === "negate") { // selects only the negate button
             if (currentNumber.textContent === "0") {
-                console.error("intentional error");
+                console.error("intentional error"); 
             } else {
                 if (!isMinus) {
-                    negative.textContent = "-"
+                    negative.textContent = "-";
+                    if (hasTyped === 0) {
+                        varB = parseFloat(varACopy);
+                        varBText = `negate(${varB})`
+                        currentNumber.textContent = parseFloat(varACopy);
+                        numberHistory.textContent = varAText + " " + chosenOperator + " " + varBText;
+                    }
                     isMinus = true;
                 } else {
                     negative.textContent = "";
+                    if (hasTyped === 0) {
+                        varB = parseFloat(varACopy);
+                        varBText = `negate(${varB})`
+                        currentNumber.textContent = parseFloat(varACopy) * -1;
+                        numberHistory.textContent = varAText + " " + chosenOperator + " " + varBText;
+                    }
                     isMinus = false;
                 }
             }
         }
 
         for (let i in numbers) {
-            if (currentNumber.textContent === "0" && currId !== "negate") {
+            if (currentNumber.textContent === "0" && event.target.id !== "negate") {
                 currentNumber.textContent = ""; // for the sake of changing the number from 0 to any
             }
-            if (currId == numbers[i]) {
+            if (event.target.id == numbers[i]) {
                 currentNumber.textContent += event.target.id;
                 if (currentNumber.textContent.length > 15) { // set maximum length
                     console.log("stahp");
@@ -70,15 +81,16 @@ function inputNumber (event) {
                 clear();
                 evalMode = false;
             }
+            if (hasTyped === 0) {
+                isMinus = false;
+                hasTyped++;
+            }
         }
 
         if (!eventFire) {
-            varA = negative.textContent + currentNumber.textContent;
-            varACopy = negative.textContent + currentNumber.textContent; // mainly use for evaluate only
-            varAText = negative.textContent + currentNumber.textContent; // mainly used to display the history after adding number 
+            varA = varACopy = varAText = negative.textContent + currentNumber.textContent;
         } else {
-            varB = negative.textContent + currentNumber.textContent;
-            varBText = negative.textContent + currentNumber.textContent;
+            varB = varBText = negative.textContent + currentNumber.textContent;
             placeHolderNumber.replaceWith(currentNumber);
         }
     }
@@ -102,8 +114,8 @@ numButtons.addEventListener("click", inputNumber);
 
 const opButton = document.querySelectorAll(".operation-button"); // optimize this later
 
+let chosenOperator = "";
 let currentOperator = "";
-let operator = "";
 
 let result = "";
 
@@ -116,18 +128,18 @@ function operate (event) {
     numButtons.addEventListener("click", inputNumber);
     decimalButton.addEventListener("click", addDecimal);
 
+    hasTyped = 0;
     if (condition) {
         for (let i in operatorsArr) {
             if (event.target.id == operatorsArr[i]) {
-                eventFire = true; // after any operator is pressed, switches to varB (and stays on varB)
-                if (currentOperator !== "") { // checks first if current operator has either +, 0 , *, / or %
-                    operator = currentOperator; // assigns a previous variable called 'operator' to current operator
+                eventFire = true; // after any currentOperator is pressed, switches to varB (and stays on varB)
+                if (chosenOperator !== "") { // checks first if current currentOperator has either +, 0 , *, / or %
+                    currentOperator = chosenOperator; // assigns a previous variable called 'currentOperator' to current currentOperator
                 }        
 
-                currentOperator = event.target.id; // set the current operator variable with either +, -, *, /, or %
+                chosenOperator = event.target.id; // set the current currentOperator variable with either +, -, *, /, or %
 
-                if (varA === "") { 
-                    console.log("executes");
+                if (varA === "") {
                     varA = 0;
                     varAText = 0; // if we don't input anything at all, just assume varA is 0
                     placeHolderNumber.textContent = "0"; // placeholder number set to 0
@@ -136,63 +148,63 @@ function operate (event) {
 
                 currentNumber.textContent = ""; // set current number to nothing, so we can input varB
 
-                if (evalMode) { // preventing from adding / substracting the result with varB after pressing any operator
+                if (evalMode) { // preventing from adding / substracting the result with varB after pressing any currentOperator
                     varB = "";
                     evalMode = false; // disabling evaluate
                 }
 
-                varACopy = negative.textContent + parseFloat(varA) + parseFloat(varB); // for evaluate only
+                varAEval = negative.textContent + parseFloat(varA) + parseFloat(varB); // for evaluate only
                 if (varB === "") {
-                    varACopy = negative.textContent + parseFloat(varA);
+                    varAEval = negative.textContent + parseFloat(varA); // also for evaluate only
                 }
-                
-                // the operation
                 if (varB !== "") {
+                    // the currentOperator
                     let a = parseFloat(varA);
                     let b = parseFloat(varB);
 
-                    if (operator === "+") {
-                        result = a + b;
-                    } else if (operator === "-") {
-                        result = a - b;
-                    } else if (operator === "x") {
-                        result = a * b;
-                    } else if (operator === "/") {
-                        if (b === 0) {
-                            opButton.forEach(item => {
-                                item.removeEventListener("click", operate);
-                                item.style.backgroundColor = "#242424";
-                            });
-                            evaluateButton.removeEventListener("click", evaluate);
-                            exponent.forEach(item => {
-                                item.removeEventListener("click", addExponent);
-                                item.style.backgroundColor = "#242424";
-                            });
+                    switch (currentOperator) {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "x":
+                            result = a * b;
+                            break;
+                        case "/":
+                            if (b === 0) {
+                                opButton.forEach(item => {
+                                    item.removeEventListener("click", operate);
+                                    item.style.backgroundColor = "#242424";
+                                });
+                                evaluateButton.removeEventListener("click", evaluate);
+                                exponent.forEach(item => {
+                                    item.removeEventListener("click", addExponent);
+                                    item.style.backgroundColor = "#242424";
+                                });
 
-                            numberHistory.textContent = varAText + " " + operator + " " + varB + " " + currentOperator + " ";
-                            placeHolderNumber.textContent = "Can't divide by zero";
-                            currentNumber.replaceWith(placeHolderNumber);
+                                numberHistory.textContent = varAText + " " + currentOperator + " " + varB + " " + chosenOperator + " ";
+                                placeHolderNumber.textContent = "Can't divide by zero";
+                                currentNumber.replaceWith(placeHolderNumber);
 
-                            isError = true;
-                            
-                        } else {
-                            result = a / b; 
+                                isError = true;
+                            } else {
+                                result = a / b;
+                            }
+                            break;
                         }
-                    }
-                    console.log(varA, varB);
                     varA = result;
                     varAText = result;
                     varB = "";
                 }
-
+            
                 if (varA !== "") { // varA will be set to result too        
-                    numberHistory.textContent = varAText + " " + currentOperator + " "; // adds the history
+                    numberHistory.textContent = varAText + " " + chosenOperator + " "; // adds the history
                     placeHolderNumber.textContent = varA; // set the placeholder to varA or the result (first time is always varA)
                     currentNumber.replaceWith(placeHolderNumber); // replace currentnumber with placeholder
                     currentNumber.textContent = ""; // empties the current number to input varB
                 }
-
-                isMinus = false; // each time operator is pressed, automatically remove negative.
                 negative.textContent = "";
             }
         }
@@ -204,46 +216,62 @@ opButton.forEach((item) => {
 })
 
 let exponent = document.querySelectorAll(".exponent");
+let chosenExponent = "";
 
 function addExponent (event) {
     // to do
     const exponent = ["sqr", "sqrt", "1/x"];
     for (let i in exponent) {
         if (event.target.id === exponent[i]) {
+            chosenExponent = event.target.id;
             if (!eventFire || evalMode) {
-                if (event.target.id === "sqr") {
-                    varAText = `sqr(${varA})`;
-                    varA = Math.pow(varA, 2);
-                } else if (event.target.id === "sqrt") {
-                    varAText = `sqrt(${varA})`
-                    varA = Math.sqrt(varA);
-                } else if (event.target.id === "1/x") {
-                    varAText = `1/${varA}`;
-                    varA = 1 / varA;
+                if (negative.textContent === "-") {
+                    negative.textContent = "";
                 }
-                placeHolderNumber.textContent = varA;
+                switch (chosenExponent) {
+                    case "sqr":
+                        varAText = `sqr(${varA})`;
+                        varACopy = varA = Math.pow(varA, 2);
+                        break;
+                    case "sqrt":
+                        varAText = `sqrt(${varA})`
+                        varACopy = varA = Math.sqrt(varA);
+                        break;
+                    case "1/x":
+                        varAText = `1/${varA}`;
+                        varACopy = varA = 1 / varA;
+                        break;
+                }
+                currentNumber.textContent = placeHolderNumber.textContent = varA;
                 numberHistory.textContent = varAText;
             }
             else if (eventFire) {
-                if (event.target.id === "sqr") {
-                    varBText = `sqr(${varB})`;
-                    varB = Math.pow(varB, 2);
-                } else if (event.target.id === "sqrt") {
-                    varBText = `sqrt(${varB})`
-                    varB = Math.sqrt(varB);
-                } else if (event.target.id === "1/x") {
-                    varBText = `1/${varB}`;
-                    varB = 1 / varB;
+                if (varB === "") {
+                    varB = varACopy;
+                }
+                switch (chosenExponent) {   
+                    case "sqr":
+                        varBText = `sqr(${varB})`;
+                        varB = Math.pow(varB, 2);
+                        break;
+                    case "sqrt":
+                        varBText = `sqrt(${varB})`
+                        varB = Math.sqrt(varB);
+                        break;
+                    case "1/x":
+                        varBText = `1/${varB}`;
+                        varB = 1 / varB;
+                        break;
                 }
                 placeHolderNumber.textContent = varB;
-                numberHistory.textContent = varAText + " " + currentOperator + " " + varBText;
+                numberHistory.textContent = varAText + " " + chosenOperator + " " + varBText;
             } 
         }
     }
     if (event.target.id === "%") { 
         varB = varA * (varB / 100);
         placeHolderNumber.textContent = varB
-        numberHistory.textContent = varAText + " " + currentOperator + " " + varB;
+        numberHistory.textContent = varAText + " " + chosenOperator + " " + varB;
     }
     currentNumber.replaceWith(placeHolderNumber);
 }
@@ -255,48 +283,53 @@ let evalMode = false;
 
 function evaluate () { 
     evalMode = true; // repeatedly count with the same varB
+    negative.textContent = "";
+    isMinus = false;
     let a = parseFloat(varA);
     let b = parseFloat(varB);
-    if (currentOperator) {
+    if (chosenOperator) {
         if (varB === "") {
-            b = parseFloat(varACopy);
+            b = parseFloat(varAEval);
             varBText = b;
         }
-        if (currentOperator == "+") {
-            result = a + b;
-        } else if (currentOperator == "-") {
-            result = a - b;
-        } else if (currentOperator == "x") {
-            result = a * b;
-        } else if (currentOperator == "/") {
-            if (b === 0) {
-                opButton.forEach(item => {
-                    item.removeEventListener("click", operate);
-                    item.style.backgroundColor = "#242424";
-                });
-                evaluateButton.removeEventListener("click", evaluate);
-                exponent.forEach(item => {
-                    item.removeEventListener("click", addExponent);
-                    item.style.backgroundColor = "#242424";
-                });
-
-                b = 0;
-
-                isError = true;
-            } else {
-                result = a / b;
-            }
-        }        
+        switch (chosenOperator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "x":
+                result = a * b;
+                break;
+            case "/":
+                if (b === 0) {
+                    opButton.forEach(item => {
+                        item.removeEventListener("click", operate);
+                        item.style.backgroundColor = "#242424";
+                    });
+                    evaluateButton.removeEventListener("click", evaluate);
+                    exponent.forEach(item => {
+                        item.removeEventListener("click", addExponent);
+                        item.style.backgroundColor = "#242424";
+                    });
+    
+                    b = 0;
+    
+                    isError = true;
+                } else {
+                    result = a / b;
+                }
+                break;
+        }
         
         varA = result;
         placeHolderNumber.textContent = varA;
         
         if (isError) {
-            numberHistory.textContent = varACopy + " " + currentOperator + " " + varBText + " = ";
             placeHolderNumber.textContent = "Can't divide by zero";
-        } else {
-            numberHistory.textContent = varAText + " " + currentOperator + " " + varBText + " = ";
         } 
+        numberHistory.textContent = varAText + " " + chosenOperator + " " + varBText + " = ";
 
         varAText = result;
         currentNumber.replaceWith(placeHolderNumber);
@@ -324,8 +357,8 @@ function clear () {
     isMinus = false;
     negative.textContent = "";
     eventFire = false;
-    operator = "";
     currentOperator = "";
+    chosenOperator = "";
     currentNumber.textContent = "0";
     numberHistory.textContent = "";
     placeHolderNumber.textContent = "";
@@ -351,12 +384,12 @@ function erase () {
         currentNumber.textContent = "0";
     }
     if (!eventFire) {
-        varA = currentNumber.textContent;
-        varACopy = currentNumber.textContent;
-        varAText = currentNumber.textContent;
+        varA = negative.textContent + currentNumber.textContent;
+        varACopy = negative.textContent + currentNumber.textContent;
+        varAText = negative.textContent + currentNumber.textContent;
     } else {
-        varB = currentNumber.textContent;
-        varBText = currentNumber.textContent;
+        varB = negative.textContent + currentNumber.textContent;
+        varBText = negative.textContent + currentNumber.textContent;
     }
     if (isError) {
         clear();
